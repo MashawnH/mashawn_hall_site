357a1bc0e85ccbf7d68880578e4f0f713f10b513
Felix Le <felixle96@gmail.com>
2020-01-19 23:12:20 -0800

    Setup views structure

---
+++ app/views/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About(props) {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <hr />
+      <h2>Intro</h2>
+      <p>
+        Non fugiat labore do qui amet consectetur irure esse. Cupidatat Lorem
+        labore enim consequat veniam enim. Enim minim sit ad eu dolor quis minim
+        officia officia officia non. Sint do eiusmod veniam ullamco sunt laborum
+        veniam consequat ex. Incididunt commodo aliquip ut incididunt nisi
+        mollit ad eiusmod reprehenderit. Aliquip aliquip pariatur nulla
+        consequat sint irure magna Lorem ex eiusmod velit ut et. Culpa occaecat
+        ullamco qui eu.
+      </p>
+    </div>
+  );
+}
---
+++ app/views/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Home(props) {
+  return (
+    <div>
+      <h1>Title</h1>
+      <p>Do incididunt consequat dolore occaecat velit.</p>
+      <hr></hr>
+      <p>
+        Dolore id velit esse ipsum irure elit dolor adipisicing. Enim et sunt
+        labore tempor excepteur nisi incididunt enim deserunt quis. Exercitation
+        commodo esse non velit eiusmod qui deserunt. Qui elit in eu dolor esse.
+        Esse deserunt exercitation fugiat ullamco ad voluptate. Pariatur nostrud
+        ut duis excepteur labore. Elit anim et velit adipisicing.
+      </p>
+      <p>Enim dolore velit ea dolor.</p>
+    </div>
+  );
+}
